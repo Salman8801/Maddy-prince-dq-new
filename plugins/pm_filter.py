1bf25e0197821d4e622422f5cabb9c3b49e626a4
Abdullah7278 <128809541+Abdullah7278@users.noreply.github.com>
2023-07-07 19:07:57 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -244,6 +244,8 @@ async def next_page(bot, query):
             )
     btn.insert(0, [
         InlineKeyboardButton("⚡ How To Download ⚡", url="https://t.me/Moviesbank_Corporation/101"),
+        ])
+    btn.insert(0, [
         InlineKeyboardButton("! Lᴀɴɢᴜᴀɢᴇs !", callback_data=f"select_lang#{req}")
     ])
     try:
@@ -353,6 +355,8 @@ async def language_check(bot, query):
         
         btn.insert(0, [
             InlineKeyboardButton("⚡ How To Download ⚡", url="https://t.me/Moviesbank_Corporation/101"),
+            ])
+    btn.insert(0, [
             InlineKeyboardButton("! Lᴀɴɢᴜᴀɢᴇs !", callback_data=f"select_lang#{userid}")
         ])
 
@@ -1617,6 +1621,8 @@ async def auto_filter(client, msg, spoll=False):
 
     btn.insert(0, [
         InlineKeyboardButton("⚡ How To Download ⚡", url="https://t.me/Moviesbank_Corporation/101"),
+        ])
+    btn.insert(0, [
         InlineKeyboardButton("! Lᴀɴɢᴜᴀɢᴇs !", callback_data=f"select_lang#{message.from_user.id}")
     ])
     
